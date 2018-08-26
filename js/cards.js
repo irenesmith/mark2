b4108241244c2f369906eaa83c5f1056aa11f50d
Irene Smith <irenesmith13@gmail.com>
2018-08-26 07:11:18 -0400

    Adding support for wild cards.

--- js/cards.js
+++ js/cards.js
@@ -5,8 +5,9 @@ class Card {
   constructor(rank, suit, cardImg) {
     this.rank = rank;
     this.suit = suit;
-    this.cardImgName = 'img/card' + suits[suit] + ranks[rank] + '.png';
+    this.cardImgName = '/img/card' + suits[suit] + ranks[rank] + '.png';
     this.faceUp = false;
+    this.held = false;
   }
 
   Flip() {
@@ -19,6 +20,12 @@ class Card {
 
   GetImageName() {
     return this.cardImgName;
+  }
+
+  HoldCard(state) {
+    // State is one of true - held
+    // or false - not held
+    this.held = state;
   }
 }
 
@@ -71,6 +78,162 @@ class Deck {
     // a must be equal to b
     return 0;
   }
+}
+
+class Hand {
+  constructor() {
+    this.cards = Array(5);
+  }
+
+  DealHand(deck) {
+    // Clear the hand. If we're dealing,
+    // we're starting fresh.
+    this.cards.length = 0;
+
+    // Get five new cards for the hand.
+    for(let i = 1; i <= 5; i++) {
+      let card = deck.Next();
+      this.cards.push(card);
+    }
+  }
+
+  DrawCards(deck) {
+    for(let i = 0; i < 5; i++) {
+      // If the card is not held, get put the next card here.
+      if( !this.cards[i].held ) {
+        hand[i] = deck.Next();
+      } else {
+        hand[i].HoldCard(false);
+      }
+    }  
+  }
+
+  ToggleHold(num) {
+    cards[num].HoldCard(!cards[num].held);
+  }
+
+  /* ------------------------------------------ */
+  /* The code below this point checks the hand  */
+  /* to see if the player wont. It might be     */
+  /* better if this code moved into the other   */
+  /* folder with Card and Deck?                 */
+  /* ------------------------------------------ */
+
+  CheckForWin() {
+    /* ------------------------------------------ */
+    /* Checks the hand for winning combinations.  */
+    /* Need to find a way to return a message as  */
+    /* well to tell the play what cause the win!  */
+    /* ------------------------------------------ */
+    
+    // First set all win types to false!
+      let isRoyalStraight = false;
+      let isStraight = false;
+    
+      let winnings = this.findMatching();
+
+      if(winnings > 0)
+        return winnings;
+
+      // First sort the cards in the hand, to check
+      // to see if there is a straight.
+      this.cards.sort(deck.Compare);
+    
+      // Check for a royal straight.
+      if(this.cards[0].rank == 1 && (this.cards[4].rank - this.cards[1].rank) == 3) {
+        // Yes, it's a Royal Straight
+        isRoyalStraight = true;
+        // If this isn't also a flush, we need to pay off
+        // for a straight.
+        isStraight = true;
+      } else if((this.cards[4].rank - this.cards[0].rank) === 4) {
+            isStraight = true;
+      }
+    
+      let hasFlush = isFlush(hand);
+    
+      // if we have both a royal straight and a flush
+      // then we have a royal flush.
+      if(isRoyalStraight && hasFlush(hand))
+        return 500 * curBet;
+    
+      if(isStraight && hasFlush)
+        return 40 * curBet;
+    
+      if(hasFlush)
+        return curBet * 5;
+    
+      if(isStraight)
+        return curBet * 3;
+    
+    }
+  
+    isFlush(hand) {
+      // if the suits are all the same, this is a flush
+      let suit = hand[0].suit;
+      let suitCount = 1;
+      for(let i = 1; i < 5; i++) {
+        if(hand[i].suit === suit) {
+          suitCount++;
+        } else {
+          return false;
+        }
+      }
+      return suitCount === 5;
+    }
+  
+    findMatching() {
+      let isFourKind = false;
+      let isThreeKind = false;
+      let cardCount = [];
+    
+      // Count cards by rank
+      for(let i = 0; i < 13; i++) {
+        cardCount[i] = 0;
+      }
+    
+      for(let i = 0; i < 5; i++) {
+        cardCount[hand[i].rank] += 1;
+      }
+    
+      // Four of a kind
+      if(cardCount.includes(4)) {
+        isFourKind = true;
+        if(cardCount[0] === 4) {
+          return curBet * 50;
+        } else {
+          return curBet * 25;
+        }
+      }
+    
+      if(cardCount.includes(3)) {
+        isThreeKind = true;
+      }
+    
+      let pairs = 0;
+      cardCount.forEach(function(count) {
+        if(count === 2) {
+          pairs++;
+        }
+      });
+    
+      // Full House
+      if(isThreeKind && pairs === 1) {
+        return curBet * 8;
+      }
+      
+      // Three of a kind
+      if(isThreeKind) {
+        return curBet * 2;
+      }
+    
+      // Two Pair
+      if(pairs === 2) {
+        return curBet;
+      }
+    
+      return 0;
+    }
 }
 
 function getRandomInt(max) {
--- js/poker.js
+++ js/poker.js
@@ -5,13 +5,14 @@ var credits = 1000; // You start with 1000 credits. This may change.
 
 /* Card variables */
 var deck;
-var hand = [];
+var hand;
 
 /* UI Elements Values */
 var holdEnabled = false;
 
 $(document).ready(function() {
   deck = new Deck();
+  hand = new Hand();
   $('#draw-button').text('DEAL');
   $('#num-credits').text(credits.toString());
 });
@@ -20,7 +21,7 @@ $(document).ready(function() {
 $('#draw-button').click(function() {
   if($('#draw-button').text() === 'DEAL') {
 
-    dealCards();
+    hand.DealHand(deck);
 
     // Subtract the credits from the player
     credits -= curBet;
@@ -32,15 +33,26 @@ $('#draw-button').click(function() {
     $('#draw-button').text('DRAW');
     holdEnabled = true;
     
+    for(let i = 1; i <= 5; i++) {
+      showCard(('#card-' + i), hand.cards[i - 1].GetImageName());
+    }
+
   } else if($('#draw-button').text() === 'DRAW') {
     // If the text is not 'DEAL' then it's 'DRAW'
 
-    drawCards();
+    hand.DrawCards(deck);
 
     $('#draw-button').text('DEAL');
 
+    for(let i = 1; i <= 5; i++) {
+      showCard(('#card-' + i), hand.cards[i - 1].GetImageName());
+       if(hand.cards[i - 1].held) {
+        clearHold(i);
+      }
+    }
+
     // See if the player won anything.
-    let winnings = checkForWin();
+    let winnings = hand.CheckForWin();
     credits += winnings;
 
     // Update the credits display
@@ -103,39 +115,9 @@ $('#decBet').click(function() {
   }
 });
 
-/* ---------------------------------------- */
-/* Deals a fresh hand. Replaces all of the  */
-/* cards currently in the hand with new     */
-/* cards.                                   */
-/* ---------------------------------------- */
-function dealCards() {
-    // Clear the hand. If we're dealing,
-    // we're starting fresh.
-    hand.length = 0;
-
-    // Get five new cards for the hand.
-    for(let i = 1; i <= 5; i++) {
-      let card = deck.Next();
-      hand.push(card);
-      showCard("#card-" + i, card.GetImageName());
-    }
-}
-
-function drawCards() {
-  for(let i = 1; i <= 5; i++) {
-    // If the card is not held, get put the next card here.
-    if( !($('#hold-' + i.toString()).hasClass('held')) ) {
-      let card = deck.Next();
-      showCard("#card-" + i, card.GetImageName());
-      hand[i-1] = card;
-    } else {
-      clearHold(i);
-    }
-  }
-}
-
 function showCard(cardId, cardImage) {
-  $(cardId).css('background-image', "url(" + cardImage + ")");
+  let img = "url(" + cardImage + ")";
+  $(cardId).css('background-image', img);
 }
 
 // Extract the number (which identifies the
@@ -148,124 +130,12 @@ function getIdNum(id) {
 function toggleHold(num) {
   $('#hold-' + num).toggleClass('held');
   $('#card-held-' + num).toggle();
+  hand.cards[i - 1].HoldCard(true);
 }
 
 function clearHold(num) {
   // remove the held status on a particular card
   $('#hold-' + num).removeClass('held');
   $('#card-held-' + num).hide();
-}
-
-/* ------------------------------------------ */
-/* The code below this point checks the hand  */
-/* to see if the player wont. It might be     */
-/* better if this code moved into the other   */
-/* folder with Card and Deck?                 */
-/* ------------------------------------------ */
-
-function checkForWin() {
-/* ------------------------------------------ */
-/* Checks the hand for winning combinations.  */
-/* Need to find a way to return a message as  */
-/* well to tell the play what cause the win!  */
-/* ------------------------------------------ */
-
-// First set all win types to false!
-  let isRoyalStraight = false;
-  let isStraight = false;
-  let isFlush = false;
-
-  // First sort the cards in the hand, to check
-  // to see if there is a straight.
-  hand.sort(deck.Compare);
-  // Check for a royal straight.
-  if(hand[0].rank == 1 && hand[1].rank == 9 && hand[2].rank == 10 && hand[3].rank == 11 && hand[4].rank == 12) {
-    // Yes, it's a Royal Straight
-    isRoyalStraight = true;
-    // If this isn't also a flush, we need to pay off
-    // for a straight.
-    isStraight = true;
-  } else if((hand[1].rank == (hand[0].rank + 1)) &&
-      (hand[2].rank == (hand[1].rank + 1)) &&
-      (hand[3].rank == (hand[2].rank + 1)) &&
-      (hand[4].rank == (hand[3].rank + 1))) {
-        isStraight = true;
-  }
-
-  // if the suits are all the same, this is a flush
-  if((hand[1].suit == (hand[0].suit + 1)) &&
-      (hand[2].suit == (hand[1].suit + 1)) &&
-      (hand[3].suit == (hand[2].suit + 1)) &&
-      (hand[4].suit == (hand[3].suit + 1))) {
-        isFlush = true;
-  }
-
-  // if we have both a royal straight and a flush
-  // then we have a royal flush.
-  if(isRoyalStraight && isFlush)
-    return 500 * curBet;
-
-  if(isStraight && isFlush)
-    return 40 * curBet;
-
-  if(isFlush)
-    return curBet * 5;
-
-  if(isStraight)
-    return curBet * 3;
-
-  return findMatching();
-}
-
-function findMatching() {
-  let isFourKind = false;
-  let isThreeKind = false;
-  let pairs = 0;
-  let cardCount = [];
-
-  // Count cards by rank
-  for(let i = 0; i < 13; i++) {
-    cardCount[i] = 0;
-  }
-
-  for(let i = 0; i < 5; i++) {
-    cardCount[hand[i].rank] += 1;
-  }
-
-  // Four of a kind
-  for(let i = 0; i < 13; i++) {
-    if(cardCount[i] == 4) {
-      ifFourKind = true;
-      if(i === 0) // Aces!
-        return curBet * 50;
-      else // 2 to King
-        return curBet * 25;
-    }
-
-    if(cardCount[i] == 3) {
-      isThreeKind = true;
-    }
-
-    if(cardCount[i] == 2) {
-      pairs++;
-    }
-
-  }
-
-  // Full House
-  if(isThreeKind && pairs === 1) {
-    return curBet * 8;
-  }
-  
-  // Three of a kind
-  if(isThreeKind) {
-    return curBet * 2;
-  }
-
-  // Two Pair
-  if(pairs === 2) {
-    return curBet;
-  }
-
-  return 0;
+  hand.cards[i = 1].HoldCard(false);
 }
